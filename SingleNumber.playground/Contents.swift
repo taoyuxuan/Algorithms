26947c9573f17c9a1c5411d4725197fde5ca95e1
Teresa <yunmengzhidian@126.com>
2020-05-14 09:08:16 +0800

    feat: single number 快排法

--- SingleNumber.playground/Contents.swift
+++ SingleNumber.playground/Contents.swift
@@ -61,7 +61,7 @@ let result2 = solu2.singleNumber([2,1,2,3,1,4,3,4,7])
 print(result2)
 
 
-// ******************* 解法2 利用数学公式 *********************//
+// ******************* 解法3 利用数学公式 *********************//
 /// 公式：2∗(a+b+c)−(a+a+b+b+c)=c
 class Solution3 {
     func singleNumber(_ nums: [Int]) -> Int {
@@ -83,3 +83,70 @@ class Solution3 {
 let solu3 = Solution3()
 let result3 = solu3.singleNumber([2,1,2,3,1,4,3,4,7])
 print(result3)
+
+
+
+// ******************* 解法4 快速排序 *********************//
+/// 当然，这种方法对于原题是不可取的，因为题目要求在线性时间复杂度内完成，这个的复杂度已经是O(nlogn)了；
+/// 不过也算是一种解法，正好把快速排序练练手，好久不写，生疏了。
+class Solution4 {
+    
+    var nums: [Int] = []
+    func singleNumber(_ nums: [Int]) -> Int {
+        self.nums = nums
+        var currentNum: Int = 0
+        var count: Int = 0
+        
+        quickSort(0, self.nums.count-1)
+        
+        for (i, val) in self.nums.enumerated() {
+            if i == 0 {
+                currentNum = val
+                count = 1
+            } else {
+                if currentNum == val {
+                    count += 1
+                } else if count == 1 {
+                    break;
+                } else {
+                    count = 1
+                    currentNum = val
+                }
+            }
+        }
+
+        return currentNum
+    }
+    
+    func quickSort(_ low: Int, _ high: Int) {
+        guard low < high else {
+            return
+        }
+        let midIdx = partion(low, high)
+        quickSort(low, midIdx-1)
+        quickSort(midIdx+1, high)
+    }
+    
+    func partion(_ low: Int, _ high: Int) -> Int {
+        let tmp: Int = nums[low]
+        var l: Int = low
+        var h: Int = high
+        while l < h {
+            while l < h, tmp <= nums[h] {
+                h -= 1
+            }
+            nums[l] = nums[h]
+            while l < h, nums[l] <= tmp {
+                l += 1
+            }
+            nums[h] = nums[l]
+        }
+        
+        nums[l] = tmp
+        return l
+    }
+}
+
+let solu4 = Solution4()
+let result4 = solu4.singleNumber([2,1,2,3,1,4,3,4,7])
+print(result4)
