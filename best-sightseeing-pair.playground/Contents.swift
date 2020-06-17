895e76ef75ec84712dd45c6889ba50798ee3664d
Teresa <yunmengzhidian@126.com>
2020-06-17 21:16:05 +0800

    feat: 1014. 最佳观光组合

---
+++ best-sightseeing-pair.playground/Contents.swift
@@ -0,0 +1,38 @@
+import UIKit
+
+//1014. 最佳观光组合
+//给定正整数数组 A，A[i] 表示第 i 个观光景点的评分，并且两个景点 i 和 j 之间的距离为 j - i。
+//一对景点（i < j）组成的观光组合的得分为（A[i] + A[j] + i - j）：景点的评分之和减去它们两者之间的距离。
+//返回一对观光景点能取得的最高分。
+//
+//示例：
+//输入：[8,1,5,2,6]
+//输出：11
+//解释：i = 0, j = 2, A[i] + A[j] + i - j = 8 + 5 + 0 - 2 = 11
+//
+//提示：
+//2 <= A.length <= 50000
+//1 <= A[i] <= 1000
+
+// **************************** 解法一 枚举 *************************** //
+class Solution {
+    func maxScoreSightseeingPair(_ A: [Int]) -> Int {
+        let count: Int = A.count
+        if count == 0 {
+            return 0
+        }
+        var ans: Int = 0
+        var maxSum: Int = A[0] + 0
+        for i in 0..<count {
+            if i == 0 {
+                continue
+            }
+            ans = max(ans, maxSum + A[i] - i)
+            maxSum = max(maxSum, A[i] + i)
+        }
+        return ans
+    }
+}
+let solu = Solution()
+let result = solu.maxScoreSightseeingPair([8,1,5,2,6])
+print(result)
